25a152b523b2ff6d2984d8628513fb004c654d52
Cian O'Sullivan <inventer100@gmail.com>
2020-05-23 15:18:24 +0100

    Add line splitter

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFSIZE 1024
+#define ARGSBUFSIZE 64
+#define DELIMS " \t\r\n\a"
 
 char* readline() {
     int bufSize = BUFSIZE;
@@ -29,12 +32,43 @@ char* readline() {
     return buffer;
 }
 
+char** splitlines(char* line) {
+    int bufSize = ARGSBUFSIZE;
+    int position = 0;
+    char** arguments = malloc(sizeof(char*) * bufSize);
+    char* argument;
+
+    argument = strtok(line, DELIMS);
+
+    while (argument != NULL) {
+        arguments[position++] = argument;
+
+        if (position >= bufSize) {
+            bufSize += ARGSBUFSIZE;
+            arguments = realloc(arguments, sizeof(char*) * bufSize);
+        }
+
+        argument = strtok(NULL, DELIMS);
+    }
+
+    arguments[position] = NULL;
+    return arguments;
+}
+
+bool execute(char** arguments) {
+    // Execute
+}
+
 void loop() {
     bool running = true;
     char* line;
+    char** arguments;
+
     while (running) {
         printf("> ");
         line = readline();
+        arguments = splitlines(line);
+        running = execute(arguments);
         printf("String entered: %s\n", line);
     }
 
